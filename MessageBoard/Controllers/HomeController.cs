aeaff93538b6e914b7993d355d3f5e15a2318e6b
Jon Bachelor <jon_bachelor@me.com>
2014-11-24 16:16:54 -0800

    Beginning [Creating Content].[Routes, views and Links]

--- MessageBoard/Controllers/HomeController.cs
+++ MessageBoard/Controllers/HomeController.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Web;
 using System.Web.Mvc;
+using MessageBoard.Models;
+using MessageBoard.Services;
 
 namespace MessageBoard.Controllers
 {
     public class HomeController : Controller
     {
+        private IMailService _mail;
+
+        public HomeController(IMailService mailService)
+        {
+            _mail = mailService;
+        }
         public ActionResult Index()
         {
             ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
@@ -26,6 +37,34 @@ namespace MessageBoard.Controllers
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactModel contactModel)
+        {
+            var svc = new MailService();
+            var msg = string.Format("Comment From:  {1}{0}Email:{2}{0}Website:  {3}{0}Comment{4}{0}"
+                , Environment.NewLine
+                , contactModel.Name
+                , contactModel.Email
+                , contactModel.Website
+                , contactModel.Comment);
+
+            svc.SendMail("noreply@jjb.com", "someone@jjb.com", "Website Contact", msg);
+
+            ViewBag.MailSent = true;
+
+            if (_mail.SendMail("noreply@jjbl.com", "foo@jjb1.com", "Website Contract", msg))
+            {
+                ViewBag.MailSent = true;
+            }
+
+            return View();
+        }
+
+        public ActionResult MyMessages()
+        {
             return View();
         }
     }
