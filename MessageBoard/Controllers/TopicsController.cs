13ec620cb34287812f443a9540b4e0549bbff84d
Jon Bachelor <jon_bachelor@me.com>
2014-12-01 16:02:27 -0800

    Almost through [Building the API].[Your First API Controller]

---
+++ MessageBoard/Controllers/TopicsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MessageBoard.Data;
+
+namespace MessageBoard.Controllers
+{
+    public class TopicsController : ApiController
+    {
+        private IMessageBoardRepository _repo;
+
+        public TopicsController(IMessageBoardRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public IEnumerable<Topic> Get()
+        {
+            return _repo.GetTopics()
+                .OrderByDescending(t => t.Created)
+                .Take(50);
+        } 
+    }
+}
