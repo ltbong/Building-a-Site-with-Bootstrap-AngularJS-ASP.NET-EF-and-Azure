ff260f990c4e127ab969cba33a58ab977b794600
Jon Bachelor <jon_bachelor@me.com>
2014-12-03 15:54:04 -0800

    Finished [Angular JS Basics].

---
+++ MessageBoard/js/home-index.js
@@ -0,0 +1,22 @@
+﻿// home-index.js
+
+var app = angular.module("myapp", []);
+
+app.controller("homeIndexController", function ($scope, $http) {
+    $scope.data = [];
+    $scope.isBusy = true;
+
+    $http.get("/api/v1/topics?includeReplies=true")
+        .then(function (result) {
+            // Success fn
+            $scope.dataCount = result.data.length;
+            angular.copy(result.data, $scope.data);
+        },
+            function () {
+                // Failure fn
+                alert("Could not load topics");
+            })
+        .then(function () {
+            $scope.isBusy = false;
+        });
+});
